a0519e2e74619b1180c9eafd81f1ab214aef7239
Francesco Torchia <francesco.torchia@suse.com>
2023-11-07 20:19:31 +0100

    first commit
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+declare var require: any;
+
 import { Component } from '@angular/core';
+const amqLib = require('node_modules/@cloudamqp/amqp-client/dist/amqp-websocket-client.mjs');
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amqp-test';
+  amqp = null;
+  textarea: any;
+  input: any;
+
+  async ngOnInit() {
+    this.textarea = document.getElementById("textarea")
+    this.input = document.getElementById("message")
+
+    const amqp = new amqLib.AMQPWebSocketClient('ws://localhost:15670', "/", "admin", "password")
+
+    try {
+
+      const conn = await amqp.connect()
+      const ch = await conn.channel()
+
+      await ch.exchangeDeclare(
+        "logs_topic", // name
+        "topic",      // type
+      )
+
+      const q = await ch.queueDeclare();
+
+      await ch.queueBind(q.Name, "logs_topic", "pippo");
+
+      ch.basicConsume(q.name, {noAck: true}, (msg: any) => {
+        console.log(msg)
+      })
+
+      // const consumer = await q.subscribe({noAck: true}, (msg: any) => {
+      //   console.log(msg)
+      // })
+    } catch (err) {
+      console.error("Error", err);
+    }
+  }
 }
