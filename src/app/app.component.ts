a1f9821dd4bdf252e114806f607e7ca4f728a11c
Francesco Torchia <francesco.torchia@suse.com>
2023-11-08 14:58:59 +0100

    get auth params
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 declare var require: any;
 
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 const amqLib = require('node_modules/@cloudamqp/amqp-client/dist/amqp-websocket-client.mjs');
 
 @Component({
@@ -9,40 +11,51 @@ const amqLib = require('node_modules/@cloudamqp/amqp-client/dist/amqp-websocket-
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'amqp-test';
   amqp = null;
   textarea: any;
   input: any;
 
-  async ngOnInit() {
-    this.textarea = document.getElementById("textarea")
-    this.input = document.getElementById("message")
+  constructor(private http: HttpClient) {}
 
-    const amqp = new amqLib.AMQPWebSocketClient('ws://localhost:15670', "/", "admin", "password")
+  ngOnInit() {
 
-    try {
+    this.http.get('/monitor-operator/auth').subscribe(async (params: any) => {
 
-      const conn = await amqp.connect()
-      const ch = await conn.channel()
+      const { rabbitmqPath, rabbitmqUsername, rabbitmqPassword } = params;
 
-      await ch.exchangeDeclare(
-        "logs_topic", // name
-        "topic",      // type
-      )
+      const amqp = new amqLib.AMQPWebSocketClient(
+        `ws://localhost/${ rabbitmqPath }`,
+        "/",
+        rabbitmqUsername,
+        rabbitmqPassword
+      );
 
-      const q = await ch.queueDeclare();
+      try {
 
-      await ch.queueBind(q.Name, "logs_topic", "pippo");
+        const conn = await amqp.connect()
+        const ch = await conn.channel()
 
-      ch.basicConsume(q.name, {noAck: true}, (msg: any) => {
-        console.log(msg)
-      })
+        await ch.exchangeDeclare(
+          "logs_topic", // name
+          "topic",      // type
+        )
 
-      // const consumer = await q.subscribe({noAck: true}, (msg: any) => {
-      //   console.log(msg)
-      // })
-    } catch (err) {
-      console.error("Error", err);
-    }
+        const q = await ch.queueDeclare();
+
+        await ch.queueBind(q.Name, "logs_topic", "pippo");
+
+        ch.basicConsume(q.name, {noAck: true}, (msg: any) => {
+          console.log(msg)
+        })
+
+        // const consumer = await q.subscribe({noAck: true}, (msg: any) => {
+        //   console.log(msg)
+        // })
+      } catch (err) {
+        console.error("Error", err);
+      }
+    });
   }
 }
